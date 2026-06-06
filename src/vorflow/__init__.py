ff5fcac4e02f88eef902a4e25b7546003d27b29c
oscarfasanchez <oscarfasanchez@gmail.com>
2026-06-06 13:04:17 +0800

    Bump version to 0.0.2

--- src/vorflow/__init__.py
+++ src/vorflow/__init__.py
@@ -1,3 +1,11 @@
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("vorflow")
+except PackageNotFoundError:
+    # Package is not installed (e.g. running from source)
+    __version__ = "0.0.2" 
+    
 from .blueprint import ConceptualMesh
 from .engine import MeshGenerator
 from .tessellator import VoronoiTessellator
